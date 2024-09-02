7ed0589026dcb5cccc95db1be0e8b9f7f661c10a
Ignazio Neto Dell'Acqua <ignaiio123@gmail.com>
2024-09-02 13:26:38 +0200

    refactored folder

--- Pablo/Code/compileShader.bat
+++
@@ -1,3 +0,0 @@
-C:\VulkanSDK\1.3.283.0\Bin\glslc.exe shaders/SimpleShader.vert -o shaders/SimpleVert.spv
-C:\VulkanSDK\1.3.283.0\Bin\glslc.exe shaders/SimpleShader.frag -o shaders/SimpleFrag.spv
-pause
